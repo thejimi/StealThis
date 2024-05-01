b98a8794f1a0eb3e42093224aef2c73ce5d337bd
thejimi <pomus@t.pl>
2024-05-01 22:51:23 +0200

    Structure + YouTube & Instagram downloading completed

---
+++ download.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const router = express.Router()
+
+const ytdl = require('ytdl-core')
+
+const timeLog = (req, res, next) => {
+    console.log("Download requested")
+    next()
+}
+
+router.use(timeLog)
+
+router.get('/', (req, res) => {
+    res.redirect('/info')
+})
+
+router.get('/youtube/:id', async (req, res) => {
+    if(!ytdl.validateID(req.params.id)) return res.render('error', {text:`Invalid YouTube Video`})
+
+    let info = await ytdl.getInfo(req.params.id);
+
+    if(req.query.formatsJson === "true"){
+        return res.json(info.formats)
+    }
+
+    const videoandsound = []
+    info.formats.forEach(element => {
+        if(element.hasAudio && element.hasVideo){
+            videoandsound.push(element)
+        }
+    });
+
+    const videonosound = []
+    info.formats.forEach(element => {
+        if(!element.hasAudio && element.hasVideo){
+            videonosound.push(element)
+        }
+    });
+
+    const justsound = []
+    info.formats.forEach(element => {
+        if(element.hasAudio && !element.hasVideo){
+            justsound.push(element)
+        }
+    });
+
+    res.render('youtube', {video:info, videoandsound:videoandsound, videonosound:videonosound, justsound:justsound})
+})
+
+router.get('/reel/:id',async(req, res) => {
+    const { ndown } = require("nayan-media-downloader")
+    let URL = await ndown(`https://instagram.com/reel/${req.params.id}`)
+    console.log(URL)
+    res.render('instagram', {data:URL.data[0]})
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
   res.redirect("/info")
 })
@@ -15,6 +17,27 @@ app.get('/watch', (req, res) => {
         return res.redirect(`/download/youtube/${req.query.v}`)
     }
 })
+
+app.get('/:username/video/:id', (req, res) => {
+    if(req.params.username.startsWith("@")){
+      return res.redirect(`/download/tiktok/${req.params.username}/${req.params.id}`)
+    }
+})
+
+app.get('/reel/:id', (req, res) => {
+    return res.redirect(`/download/reel/${req.params.id}`)
+})
+
+app.get('/r/:subredditname/comments/:id/:title', (req, res) => {
+    return res.redirect(`/download/reddit/?url=https://www.reddit.com/r/${req.params.subredditname}/comments/${req.params.id}/${req.params.title}`)
+})
+
+app.get('/favicon', (req, res) => {
+  res.sendFile(__dirname + "/favicon.png")
+})
+
+const download = require('./download')
+app.use('/download', download)
 
 app.listen(port, () => {
   console.log(`${port}`)
