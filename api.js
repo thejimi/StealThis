aa73c39f76520b6f4d29ea9d9c92f6d804196ae9
thejimi <pomus@t.pl>
2024-05-03 15:19:07 +0200

    Discord lookup

--- api.js
+++ api.js
@@ -14,8 +14,14 @@ router.get('/', (req, res) => {
     res.redirect('/?h=api')
 })
 
-router.get('/discord_user/:id', (req, res) => {
-    
+router.get('/discord_user/:id', async (req, res) => {
+    const {discord_bot_token} = require('./config.json')
+    const { REST } = require('@discordjs/rest')
+    const { Routes } = require('discord-api-types/v10')
+    const rest = new REST({version:'10', authPrefix:'Bot'}).setToken(discord_bot_token)
+
+    const info = await rest.get(Routes.user(req.params.id))
+    res.json(info)
 })
 
 module.exports = router
